1825024f85b03bf38447c1331ac41bb15d3ebde4
Ophélie BELLISSENS <obellissens1@gmail.com>
2024-05-10 15:32:11 +0200

    Projet TS

---
+++ arkanoid-ts-startHere/src/index.ts
@@ -0,0 +1,102 @@
+import { CanvasView } from "./view/CanvasView";
+import { Ball } from "./sprites/Ball";
+import { Brick } from "./sprites/Brick";
+import { Paddle } from "./sprites/Paddle";
+import { Collision } from "./Collison";
+//images
+import PADDLE_IMAGE from './images/paddle.png';
+import BALL_IMAGE from './images/ball.png';
+// level and colors
+
+import { PADDLE_SPEED, PADDLE_WIDTH,PADDLE_HEIGHT,PADDLE_STARTX,BALL_SPEED,BALL_SIZE,BALL_STARTX,BALL_STARTY } from "./setup";
+
+//Helpers
+import { createBricks } from "./helpers";
+
+let gameOver = false;
+let score = 0;
+
+function setGameOver(view: CanvasView) {
+    view.drawInfo('Game Over !')
+    gameOver = false;
+}
+
+function setGameWin (view: CanvasView) {
+    view.drawInfo ('Game won !');
+    view.clear()
+    gameOver = false;
+}
+
+function gameLoop(
+    view: CanvasView,
+    bricks: Brick[],
+    paddle: Paddle,
+    ball: Ball,
+    collision: Collision
+) {
+    view.clear();
+    view.drawBricks(bricks);
+    view.drawSprite(paddle);
+    view.drawSprite(ball);
+    ball.moveBall();
+
+
+    if(
+        (paddle.isMovingLeft && paddle.pos.x > 0) || (paddle.isMovingRight && paddle.pos.x < view.canvas.width - paddle.width)
+    ){
+        paddle.movePaddle();
+    }
+
+    collision.checkBallCollision(ball, paddle, view);
+    const isCollidingBrick = collision.isCollidingBricks(ball, bricks)
+    if (isCollidingBrick){
+        score += 1;
+        view.drawScore(score)
+    }
+    if(ball.pos.y > view.canvas.height) gameOver = true;
+    if(bricks.length === 0) return setGameWin(view)
+    if(gameOver) return setGameOver(view);
+    
+    requestAnimationFrame(() => gameLoop(view, bricks, paddle, ball, collision));
+}
+
+function startGame(view: CanvasView){
+    console.log("HELLO");
+    score = 0;
+    view.drawInfo('');
+    view.drawScore(0);
+    // Create the collision instance
+    const collision = new Collision();
+    //Create all bricks
+    const bricks = createBricks();
+    // Create Ball
+    const ball = new Ball(
+        BALL_SPEED,
+        BALL_SIZE,
+        {x: BALL_STARTX, y: BALL_STARTY},
+        BALL_IMAGE
+    )
+    // Create Paddle
+    const paddle = new Paddle(
+        PADDLE_SPEED,
+        // Ne pas inverser Width et Height
+        PADDLE_WIDTH,
+        PADDLE_HEIGHT,
+        {
+            x: PADDLE_STARTX,
+            y: view.canvas.height - PADDLE_HEIGHT - 5
+        },
+        PADDLE_IMAGE
+    )
+    
+
+    gameLoop(view, bricks, paddle, ball, collision);
+
+}
+
+// Create a new view
+const view = new CanvasView('#playField');
+view.initStartButton(startGame);
+
+
+
